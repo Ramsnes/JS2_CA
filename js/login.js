af6c55a906a1a41bcf3d753913f31f1958a65f72
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-11-30 18:01:18 +0100

    fixed login and reg details

--- js/login.js
+++ js/login.js
@@ -1,25 +1,27 @@
 const loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Stops default submission behaviour
+  event.preventDefault(); // Stops default submission behavior
 
-  // Gets username/password from form
+  // Gets username, email and password from the form
   const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  // Calls login function(hoisted) with username/password
-  login(username, password);
+  // Calls the login function with email/password
+  login(username, email, password);
 });
 
-function login(username, password) {
+function login(username, email, password) {
   // Fetch API to make POST request to the login endpoint
-  fetch("/api/login", {
+  fetch("/social/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    // stringifies login details
+    // Stringifies login details
     body: JSON.stringify({
       username: username,
+      email: email,
       password: password,
     }),
   })
@@ -27,19 +29,19 @@ function login(username, password) {
       if (!response.ok) {
         throw new Error("Login failed");
       }
-      return response.json(); // implicite else
+      return response.json(); // Implicit else
     })
     .then((data) => {
       // API response includes a JWT token
-      const token = data.token;
+      const token = data.accessToken; // Adjust the property name based on the actual API response
 
-      // Stores token in local storage
+      // Stores the token in local storage
       localStorage.setItem("token", token);
 
-      // Optional redirection of user to a page
-      // window.location.href = "/pageName";
+      //redirection of the user to a page
+      window.location.href = "/profile/index.html";
     })
-    // Handles display and error to user
+    // Handles display and error to the user
     .catch((error) => {
       console.error("Login error:", error);
     });
